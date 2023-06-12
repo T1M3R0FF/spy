3225dd6957142ba10aa9e55a05201381e80d3f45
T1M3R0FF <118768348+T1M3R0FF@users.noreply.github.com>
2023-06-12 15:45:04 +0400

    не обрабатывает двух онлайнов одновременно

--- spy.py
+++ spy.py
@@ -5,15 +5,16 @@ import time
 app = Client(name='my_acc', api_id=api_id, api_hash=api_hash)
 online_start_time = None
 
-users = ['@Markthewriter']
+users = {
+    'Markthewriter': {},
+    'clown000001': {}
+}
 
-try:
+with app:
     for user in users:
-        chat = app.get_chat(user)
-        chat_id = chat.id
-        user_id = app.get_users(chat_id)
-except Exception as e:
-    print("Произошла ошибка:", e)
+        chel = app.get_users(user)
+        user_id = chel.id
+        users[user].update({user_id: 0})
 
 
 def online_handler(client, update):
@@ -24,12 +25,16 @@ def online_handler(client, update):
         print(f'{user} вошел в онлайн!')
     elif str(update.status) == "UserStatus.OFFLINE" and update.id != client.get_me().id:
         if online_start_time is not None:
+
             struct = time.localtime(online_start_time)
             good_format = time.strftime('%d.%m.%Y %H:%M', struct)
+
             online_end_time = time.time()
             online_duration = online_end_time - online_start_time
+
             struct1 = time.localtime(online_end_time)
             good_format1 = time.strftime('%d.%m.%Y %H:%M', struct1)
+
             if online_duration >= 60:
                 mins = online_duration // 60
                 if mins >= 60:
@@ -43,6 +48,8 @@ def online_handler(client, update):
                 hrs = 0
             print(f'{user} вышел из онлайн, зашел в {good_format}, вышел в {good_format1},'
                   f' время в сети: {hrs} ч {mins} мин {int(online_duration)} сек')
+            users[user][user_id] += int(online_duration)
+            print(users)
         else:
             print('Юзер пока не заходил в сеть')
         online_start_time = None
