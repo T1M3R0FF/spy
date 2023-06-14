c20165add9ff1c54864a00ab1926e6555c29d0b7
T1M3R0FF <118768348+T1M3R0FF@users.noreply.github.com>
2023-06-14 13:13:20 +0400

    нормально сохраняется в бд заход и выход из сети пользователей, только если это происходит не одновременно. выводится последний онлайн, если юзер вданный момент оффлайн

--- spy.py
+++ spy.py
@@ -1,3 +1,4 @@
+import sqlite3
 from pyrogram.enums import UserStatus
 from pyrogram import Client
 from data import *
@@ -5,57 +6,101 @@ import time
 
 app = Client(name='my_acc', api_id=api_id, api_hash=api_hash)
 
-online_start_time = None
-
 users = ['Markthewriter', 'Jimmythedoc']
+online_time = {}
+
+for user in users:
+    online_time.update({user: {0.0: 0.0}})  # [online_start, online_end]
 
 
 def is_user_online(username):
     user = app.get_users(username)
-    if user.status == UserStatus.ONLINE:
-        return True
-    elif user.status == UserStatus.OFFLINE:
-        return False
+    return user.status is UserStatus.ONLINE
 
 
-"""def online_handler(client, update):
-    global online_start_time
-    if str(update.status) == "UserStatus.ONLINE" and update.id != client.get_me().id:
-        if online_start_time is None:
-            online_start_time = time.time()
-        print(f'{user} вошел в онлайн!')
-    elif str(update.status) == "UserStatus.OFFLINE" and update.id != client.get_me().id:
-        if online_start_time is not None:
+def save_to_database(username, online_start, online_end):
+    conn = sqlite3.connect('online_time.db')
+    c = conn.cursor()
 
-            struct = time.localtime(online_start_time)
-            good_format = time.strftime('%d.%m.%Y %H:%M', struct)
+    c.execute("INSERT INTO online_time (username, online_start, online_end) VALUES (?, ?, ?)",
+              (username, online_start, online_end))
 
-            online_end_time = time.time()
-            online_duration = online_end_time - online_start_time
+    conn.commit()
+    conn.close()
 
-            struct1 = time.localtime(online_end_time)
-            good_format1 = time.strftime('%d.%m.%Y %H:%M', struct1)
 
-            if online_duration >= 60:
-                mins = online_duration // 60
-                if mins >= 60:
-                    hrs = mins // 60
-                    mins -= hrs * 60
+def execute_last_seen(username):
+    conn = sqlite3.connect('online_time.db')
+    c = conn.cursor()
+
+    c.execute('SELECT * FROM online_time WHERE username = ? ORDER BY id DESC LIMIT 1', (username,))
+
+    row = c.fetchone()
+    if row:
+        last_online_start = row[2]
+        last_seen = row[3]
+        return last_online_start, last_seen
+
+
+def is_database_empty():
+    conn = sqlite3.connect('online_time.db')
+    c = conn.cursor()
+
+    c.execute('SELECT COUNT(*) FROM online_time')
+    result = c.fetchone()
+    count = result[0]
+
+    conn.close()
+
+    return count == 0
+
+
+def create_table_if_not_exists():
+    conn = sqlite3.connect('online_time.db')
+    c = conn.cursor()
+
+    c.execute('''CREATE TABLE IF NOT EXISTS online_time
+              (id INTEGER PRIMARY KEY AUTOINCREMENT,
+               username TEXT,
+               online_start REAL,
+               online_end REAL)''')
+
+    conn.commit()
+    conn.close()
+
+
+def online_handler():
+    global online_time
+    create_table_if_not_exists()
+    while True:
+        for username in users:
+            if is_user_online(username):
+                online_time[username][0] = time.time()
+                print(f'{username} онлайн!')
+            elif not is_user_online(username):
+                if not online_time[username][0]:
+                    if not is_database_empty():
+                        start, last_seen = execute_last_seen(username)
+                        print(f'{username} пока не заходил в сеть. Последний раз был в сети: {last_seen}')
+                    else:
+                        print(f'{username} пока не заходил в сеть.')
                 else:
-                    hrs = 0
-                online_duration -= mins * 60
-            else:
-                mins = 0
-                hrs = 0
-            print(f'{user} вышел из онлайн, зашел в {good_format}, вышел в {good_format1},'
-                  f' время в сети: {hrs} ч {mins} мин {int(online_duration)} сек')
-        else:
-            print('Юзер пока не заходил в сеть')
-        online_start_time = None"""
+                    online_time[username][1] = time.time()
+
+                    struct = time.localtime(online_time[username][0])
+                    good_format = time.strftime('%d.%m.%Y %H:%M:%S', struct)
+
+                    struct1 = time.localtime(online_time[username][1])
+                    good_format1 = time.strftime('%d.%m.%Y %H:%M:%S', struct1)
+
+                    print(f'{username} вышел из онлайн, зашел в {good_format}, вышел в {good_format1}')
+
+                    save_to_database(username, good_format, good_format1)
+                    online_time[username].update({0.0: 0.0})
+        time.sleep(1)
 
 
-#app.on_user_status('UserStatus.ONLINE')(online_handler)
 with app:
-    for username in users:
-        print(is_user_online(username))
+    online_handler()
+
 app.run()
