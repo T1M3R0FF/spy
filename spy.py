6dcb9d20f434c4ad5b888d6d8cf8c19790cdabae
T1M3R0FF <118768348+T1M3R0FF@users.noreply.github.com>
2023-06-15 00:35:05 +0400

    в бд корректно сохраняется одновременный выход нескольких пользователей из онлайн, скрипт рабочий, осталось настроить удобное для пользователя сохранение в бд

--- spy.py
+++ spy.py
@@ -9,8 +9,8 @@ app = Client(name='my_acc', api_id=api_id, api_hash=api_hash)
 users = ['Markthewriter', 'Jimmythedoc']
 online_time = {}
 
-for user in users:
-    online_time.update({user: {0.0: 0.0}})  # [online_start, online_end]
+for person in users:
+    online_time.update({person: {0.0: 0.0}})  # [online_start, online_end]
 
 
 def is_user_online(username):
@@ -36,17 +36,21 @@ def execute_last_seen(username):
     c.execute('SELECT * FROM online_time WHERE username = ? ORDER BY id DESC LIMIT 1', (username,))
 
     row = c.fetchone()
+
+    conn.close()
+
     if row:
         last_online_start = row[2]
         last_seen = row[3]
+
         return last_online_start, last_seen
 
 
 def is_database_empty():
     conn = sqlite3.connect('online_time.db')
     c = conn.cursor()
-
     c.execute('SELECT COUNT(*) FROM online_time')
+
     result = c.fetchone()
     count = result[0]
 
@@ -74,13 +78,13 @@ def online_handler():
     create_table_if_not_exists()
     while True:
         for username in users:
-            if is_user_online(username):
+            if is_user_online(username) and not online_time[username][0]:
                 online_time[username][0] = time.time()
                 print(f'{username} онлайн!')
             elif not is_user_online(username):
                 if not online_time[username][0]:
                     if not is_database_empty():
-                        start, last_seen = execute_last_seen(username)
+                        _, last_seen = execute_last_seen(username)
                         print(f'{username} пока не заходил в сеть. Последний раз был в сети: {last_seen}')
                     else:
                         print(f'{username} пока не заходил в сеть.')
