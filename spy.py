519ab9c2015120529b9ae6a6aa875c016d589705
T1M3R0FF <118768348+T1M3R0FF@users.noreply.github.com>
2023-06-17 14:08:52 +0400

    обновление тз с сохранением времени и 0 или 1 в зависимости от онлайна

--- spy.py
+++ spy.py
@@ -3,14 +3,11 @@ from pyrogram.enums import UserStatus
 from pyrogram import Client
 from data import *
 import time
+import schedule
 
 app = Client(name='my_acc', api_id=api_id, api_hash=api_hash)
 
-users = ['Markthewriter', 'Jimmythedoc']
-online_time = {}
-
-for person in users:
-    online_time.update({person: {0.0: 0.0}})  # [online_start, online_end]
+users = ['shalimov_k', 'reilon', 'Jimmythedoc']
 
 
 def is_user_online(username):
@@ -18,32 +15,14 @@ def is_user_online(username):
     return user.status is UserStatus.ONLINE
 
 
-def save_to_database(username, online_start, online_end):
+def save_to_database(username, time, is_online):
     conn = sqlite3.connect('online_time.db')
     c = conn.cursor()
 
-    c.execute("INSERT INTO online_time (username, online_start, online_end) VALUES (?, ?, ?)",
-              (username, online_start, online_end))
+    c.execute("INSERT INTO online_time (username, time, is_online) VALUES (?, ?, ?)",
+              (username, time, is_online))
 
     conn.commit()
-    conn.close()
-
-
-def execute_last_seen(username):
-    conn = sqlite3.connect('online_time.db')
-    c = conn.cursor()
-
-    c.execute('SELECT * FROM online_time WHERE username = ? ORDER BY id DESC LIMIT 1', (username,))
-
-    row = c.fetchone()
-
-    conn.close()
-
-    if row:
-        last_online_start = row[2]
-        last_seen = row[3]
-
-        return last_online_start, last_seen
 
 
 def is_database_empty():
@@ -53,9 +32,6 @@ def is_database_empty():
 
     result = c.fetchone()
     count = result[0]
-
-    conn.close()
-
     return count == 0
 
 
@@ -66,45 +42,27 @@ def create_table_if_not_exists():
     c.execute('''CREATE TABLE IF NOT EXISTS online_time
               (id INTEGER PRIMARY KEY AUTOINCREMENT,
                username TEXT,
-               online_start REAL,
-               online_end REAL)''')
+               time REAL,
+               is_online INTEGER)''')
 
     conn.commit()
-    conn.close()
 
 
 def online_handler():
-    global online_time
     create_table_if_not_exists()
-    while True:
-        for username in users:
-            if is_user_online(username) and not online_time[username][0]:
-                online_time[username][0] = time.time()
-                print(f'{username} онлайн!')
-            elif not is_user_online(username):
-                if not online_time[username][0]:
-                    if not is_database_empty():
-                        _, last_seen = execute_last_seen(username)
-                        print(f'{username} пока не заходил в сеть. Последний раз был в сети: {last_seen}')
-                    else:
-                        print(f'{username} пока не заходил в сеть.')
-                else:
-                    online_time[username][1] = time.time()
+    for username in users:
+        struct = time.localtime(time.time())
+        good_format = time.strftime('%d.%m.%Y %H:%M:%S', struct)
 
-                    struct = time.localtime(online_time[username][0])
-                    good_format = time.strftime('%d.%m.%Y %H:%M:%S', struct)
-
-                    struct1 = time.localtime(online_time[username][1])
-                    good_format1 = time.strftime('%d.%m.%Y %H:%M:%S', struct1)
-
-                    print(f'{username} вышел из онлайн, зашел в {good_format}, вышел в {good_format1}')
-
-                    save_to_database(username, good_format, good_format1)
-                    online_time[username].update({0.0: 0.0})
-        time.sleep(1)
+        is_online = int(is_user_online(username))
+        save_to_database(username, good_format, is_online)
+        print(username, is_online)
 
 
 with app:
-    online_handler()
+    schedule.every(1).minutes.do(online_handler)
 
-app.run()
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
