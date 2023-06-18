b6c08b94c3c0b4991db56524f21664e96aec9f1e
T1M3R0FF <118768348+T1M3R0FF@users.noreply.github.com>
2023-06-18 21:41:00 +0400

    сделал запись статуса в указанную ячейку 'b' с инкрементом индекса после записи, нужно сделать, чтобы это 'b' находилось через наименование колонки в таблице

--- spy.py
+++ spy.py
@@ -4,8 +4,15 @@ from pyrogram import Client
 from data import *
 import time
 import schedule
+import gspread
+from google.oauth2 import service_account
 
+cell_index = 2
 app = Client(name='my_acc', api_id=api_id, api_hash=api_hash)
+
+# Установка учетных данных и доступа к Google Sheets
+scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+credentials = service_account.Credentials.from_service_account_file('cred.json', scopes=scope)
 
 users = ['shalimov_k', 'reilon', 'Jimmythedoc']
 
@@ -48,6 +55,28 @@ def create_table_if_not_exists():
     conn.commit()
 
 
+def sheet_insert(status):
+    global cell_index
+    username = 'shalimov_k'
+
+    spreadsheet = gspread.authorize(credentials)
+    worksheet = spreadsheet.open('online_spy').worksheet('first_list')
+    # Запись данных в именованный столбец
+    column_name = username
+    data = status  # Здесь можно указать ваши данные
+
+    # Получение диапазона ячеек для именованного столбца
+
+    range_start = f'B{cell_index}'
+    named_range = worksheet.range(range_start)
+    # Запись данных в ячейки диапазона
+    for _, cell in enumerate(named_range):
+        cell.value = data
+        worksheet.update_cells(named_range)
+    print('updated')
+    cell_index += 1
+
+
 def online_handler():
     create_table_if_not_exists()
     for username in users:
@@ -56,13 +85,12 @@ def online_handler():
 
         is_online = int(is_user_online(username))
         save_to_database(username, good_format, is_online)
-        print(username, is_online)
+        sheet_insert(is_online)
 
 
 with app:
-    schedule.every(1).minutes.do(online_handler)
+    schedule.every(5).seconds.do(online_handler)
 
     while True:
         schedule.run_pending()
         time.sleep(1)
-
