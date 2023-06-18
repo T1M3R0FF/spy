663cc381a00a1892042278f5643d5371a0e759b2
T1M3R0FF <118768348+T1M3R0FF@users.noreply.github.com>
2023-06-19 02:41:56 +0400

    добавлены функции именования колонок и закрепления столбцов за этими именами, куда корректно вносятся статусы. Можно менять список как угодно, но в пределах 26 человек(шире гугл таблицу нельзясделать(или можно)) плюс добавлена очистка таблицы после завершения суток

--- spy.py
+++ spy.py
@@ -7,19 +7,30 @@ import schedule
 import gspread
 from google.oauth2 import service_account
 
-cell_index = 2
+cell_name_flag = False
 app = Client(name='my_acc', api_id=api_id, api_hash=api_hash)
 
 # Установка учетных данных и доступа к Google Sheets
 scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
 credentials = service_account.Credentials.from_service_account_file('cred.json', scopes=scope)
 
-users = ['shalimov_k', 'reilon', 'Jimmythedoc']
+# пользователь : {буквы ячейки: номер ячейки}
+users = {
+    'shalimov_k': {
+        '': 2
+    },
+    'reilon': {
+        '': 2
+    },
+    'Jimmythedoc': {
+        '': 2
+    }
+}
 
 
 def is_user_online(username):
     user = app.get_users(username)
-    return user.status is UserStatus.ONLINE
+    return username, int(user.status is UserStatus.ONLINE)
 
 
 def save_to_database(username, time, is_online):
@@ -55,42 +66,95 @@ def create_table_if_not_exists():
     conn.commit()
 
 
-def sheet_insert(status):
-    global cell_index
-    username = 'shalimov_k'
+# запись имени в таблицу и получение буквы ячейки юзера
+def cell_name(usernames):
+    global cell_name_flag
+    spreadsheet = gspread.authorize(credentials)
+    worksheet = spreadsheet.open('online_spy').worksheet('first_list')
+    my_set = set()
 
+    for username in usernames:
+        for char in range(66, 90):
+            str_range = f'{chr(char)}1'
+            row_range = worksheet.range(str_range)
+            cell_value = [cell.value for cell in row_range]
+            if cell_value != [''] and cell_value == [f'{username}']:
+                print('already in')
+                users[username][str_range[:1]] = 2
+                del users[username]['']
+                my_set.add(username)
+                break
+            elif cell_value == ['']:
+                for _, cell in enumerate(row_range):
+                    cell.value = username
+                    worksheet.update_cells(row_range)
+                    users[username][str_range[:1]] = 2
+                    del users[username]['']
+                    print('wrote a name')
+                    my_set.add(username)
+                    break
+                break
+        # смотрим, все ли имена внесли в таблицу
+        if len(my_set) == len(users):
+            cell_name_flag = True
+
+
+def sheet_clear():
+    client = gspread.authorize(credentials)
+    spreadsheet = client.open('online_spy')
+    worksheet = spreadsheet.worksheet('first_list')
+    start_col = 2  # Начальный столбец (B)
+    end_col = 26  # Конечный столбец (Z)
+    start_row = 1  # Начальная строка
+    end_row = 289  # Конечная строка
+
+    # Очистка диапазона ячеек
+    for col in range(start_col, end_col + 1):
+        cell_range = worksheet.range(start_row, col, end_row, col)
+        for cell in cell_range:
+            cell.value = ''
+        worksheet.update_cells(cell_range)
+
+    print('Cleared')
+
+
+def sheet_insert(name, status):
     spreadsheet = gspread.authorize(credentials)
     worksheet = spreadsheet.open('online_spy').worksheet('first_list')
     # Запись данных в именованный столбец
-    column_name = username
-    data = status  # Здесь можно указать ваши данные
 
-    # Получение диапазона ячеек для именованного столбца
-
-    range_start = f'B{cell_index}'
-    named_range = worksheet.range(range_start)
-    # Запись данных в ячейки диапазона
-    for _, cell in enumerate(named_range):
-        cell.value = data
-        worksheet.update_cells(named_range)
-    print('updated')
-    cell_index += 1
+    for key in users[name]:
+        range_start = f'{key}{users[name][key]}'
+        named_range = worksheet.range(range_start)
+        # Запись данных в ячейки диапазона
+        for _, cell in enumerate(named_range):
+            cell.value = status
+            worksheet.update_cells(named_range)
+        print(users)
+        print('updated')
+        if users[name][key] < 289:
+            users[name][key] += 1
+        else:
+            print('сутки закончились')
+            users[name][key] = 2
+            sheet_clear()
 
 
 def online_handler():
     create_table_if_not_exists()
+    if not cell_name_flag:
+        cell_name(users)
     for username in users:
         struct = time.localtime(time.time())
         good_format = time.strftime('%d.%m.%Y %H:%M:%S', struct)
 
-        is_online = int(is_user_online(username))
-        save_to_database(username, good_format, is_online)
-        sheet_insert(is_online)
+        name, status = is_user_online(username)
+        save_to_database(name, good_format, status)
+        sheet_insert(name, status)
 
 
 with app:
-    schedule.every(5).seconds.do(online_handler)
-
+    schedule.every(5).minutes.do(online_handler)
     while True:
         schedule.run_pending()
         time.sleep(1)
