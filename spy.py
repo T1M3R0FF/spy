7de22beb103940122291a147dac885472d272412
T1M3R0FF <118768348+T1M3R0FF@users.noreply.github.com>
2023-06-20 17:13:36 +0400

    исправил время на 48 строке

--- spy.py
+++ spy.py
@@ -5,9 +5,8 @@ import time
 import gspread
 from google.oauth2 import service_account
 
-cell_name_flag = False
 app = Client(name='my_acc', api_id=api_id, api_hash=api_hash)
-row_index = 2
+
 # Установка учетных данных и доступа к Google Sheets
 scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
 credentials = service_account.Credentials.from_service_account_file('cred.json', scopes=scope)
@@ -28,49 +27,38 @@ def is_user_online(username):
 
 # заполнение первой строки таблицы именами
 def cell_name(usernames):
-    global cell_name_flag
     spreadsheet = gspread.authorize(credentials)
     worksheet = spreadsheet.open('online_spy').worksheet('first_list')
 
-    first_row = ['Дата и время']
+    first_row = ['Дата и время'] + usernames
 
-    for name in usernames:
-        first_row.append(name)
-
-    worksheet.insert_row(first_row, 1)
-    cell_name_flag = True
-
-    return cell_name_flag
+    worksheet.insert_row(first_row)
 
 
 # внос статусов
-def insert_row(my_list):
+def append_statuses(my_list):
     spreadsheet = gspread.authorize(credentials)
     worksheet = spreadsheet.open('online_spy').worksheet('first_list')
 
-    worksheet.insert_row(my_list, row_index)
+    worksheet.append_row(my_list)
 
 
 def online_handler():
-    global row_index
     row_list = []
     # внос времени проверки в лист со статусами
-    struct = time.localtime(time.time())
-    good_format = time.strftime('%d.%m.%Y %H:%M', struct)
+    good_format = time.strftime('%d.%m.%Y %H:%M')
     row_list.append(good_format)
-    # внос имен должен срабатывать только 1 раз
-    if not cell_name_flag:
-        cell_name(users)
+
     # формирование листа статусами
     for username in users:
         status = is_user_online(username)
         row_list.append(status)
-    insert_row(row_list)
+    append_statuses(row_list)
     print('inserted')
-    row_index += 1
 
 
 with app:
+    cell_name(users)
     while True:
         online_handler()
         time.sleep(60)
